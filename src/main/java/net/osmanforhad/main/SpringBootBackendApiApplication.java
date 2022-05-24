6cf98065d5805009eb94b493132dc17de2446e09
osmanforhad <developer.osmanforhad@gmail.com>
2022-05-24 09:18:54 +0600

    Spring Boot REST API project setup

---
+++ src/main/java/net/osmanforhad/main/SpringBootBackendApiApplication.java
@@ -0,0 +1,13 @@
+package net.osmanforhad.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApiApplication.class, args);
+	}
+
+}
