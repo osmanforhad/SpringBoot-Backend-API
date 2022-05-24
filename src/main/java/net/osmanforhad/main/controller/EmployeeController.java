2e8141b7ad4015cad17e003eb360ebb005fbb2c9
osmanforhad <developer.osmanforhad@gmail.com>
2022-05-24 23:00:45 +0600

    create get employee by id rest api

--- src/main/java/net/osmanforhad/main/controller/EmployeeController.java
+++ src/main/java/net/osmanforhad/main/controller/EmployeeController.java
@@ -3,13 +3,16 @@ package net.osmanforhad.main.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.osmanforhad.main.excaption.ResourceNotFoundException;
 import net.osmanforhad.main.model.Employee;
 import net.osmanforhad.main.repository.EmployeeRepository;
 
@@ -18,20 +21,28 @@ import net.osmanforhad.main.repository.EmployeeRepository;
 @CrossOrigin(origins = "http://localhost:3000")
 public class EmployeeController {
 
-	//inject EmployeeRepository class as dependency
+	// inject EmployeeRepository class as dependency
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
-	//get all employees
+
+	// get all employees
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
+	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
-	
-	//create employee
+
+	// create employee
 	@PostMapping("/create_employee")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
 	}
-	
+
+	// get employee by id
+	@GetMapping("/employee/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exists with id :" + id));
+		return ResponseEntity.ok(employee);
+	}
+
 }
